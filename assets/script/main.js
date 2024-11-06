d263f13a6bf38332ce7c31c0d5d6c914df303b10
yunaArai <araiyunaaa59@gmail.com>
2024-11-06 22:34:46 +0900

    “初稿”

--- assets/script/main.js
+++ assets/script/main.js
@@ -16,8 +16,4 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 }, false);
 
-document.getElementById('hamburger-menu').addEventListener('click', function () {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active'); // メニューを表示・非表示
-});
 
